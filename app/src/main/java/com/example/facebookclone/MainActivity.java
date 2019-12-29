7f91a6c35751877321e105762dcc6137d0e46f18
sumanbhujel <xnamus@outlook.com>
2019-12-29 23:27:05 +0545

    Initial commit

---
+++ app/src/main/java/com/example/facebookclone/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.facebookclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnCreate, btnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnCreate = findViewById(R.id.buttonCreate);
+        btnLogin = findViewById(R.id.buttonLogin);
+
+        btnCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
