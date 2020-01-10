8849a526511068a7578bc9db9e7e3c1595e727ab
sumanbhujel <xnamus@outlook.com>
2020-01-10 16:07:38 +0545

    postviewmodel added

--- app/src/main/java/com/example/facebookclone/ui/post/PostViewModel.java
+++ app/src/main/java/com/example/facebookclone/ui/post/PostViewModel.java
@@ -4,16 +4,16 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class PostViewModel extends ViewModel {
-
+public class PostViewModel extends ViewModel{
     private MutableLiveData<String> mText;
 
     public PostViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is post fragment");
+        mText.setValue("This is addpost fragment");
     }
 
     public LiveData<String> getText() {
         return mText;
     }
+
 }
